ae9ee4fa4195d92879c744b4d08f818cfd3b524d
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-03-06 07:29:45 +0530

    Selection_Sort.py

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -19,6 +19,5 @@ print(selection_sort(array))
 sorted_array = [5,6,7,8,9]
 print(selection_sort(sorted_array))
 
-
 reverse_sorted_array = [9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
 print(selection_sort(reverse_sorted_array))
